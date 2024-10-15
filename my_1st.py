34884efc86932abab7df465a4e85c6d21bbe2e02
Youngeunzzi <102658630+Youngeunzzi@users.noreply.github.com>
2024-10-15 09:51:32 +0900

    Update my_1st.py

--- my_1st.py
+++ my_1st.py
@@ -3,33 +3,52 @@ import streamlit as st # streamlit 라이브러리 임포트
 # 타이틀 텍스트 출력
 st.title('첫번째 웹 어플 만들기 👋')
 
-
 # 텍스트 출력
-7 st.write('# 1. Markdown 텍스트 작성하기')
-8
-9 # Markdown 문법으로 작성된 문장 출력
-10 st.markdown(
-11 '''
-12 # 마크다운 헤더1
-13 - 마크다운 목록1. **굵게** 표시
-14 - 마크다운 목록2. *기울임* 표시
-15 - 마크다운 목록2-1
-16 - 마크다운 목록2-2
-17
-18 ## 마크다운 헤더2
-19 - [네이버](https://naver.com)
-20 - [구글](https://google.com)
-21
-22 ### 마크다운 헤더3
-23 일반 텍스트
-24 '''
-25 )
-26
-27 # DataFrame 출력
-28 import pandas as pd # pandas 라이브러리 임포트
-29
-30 st.write('# 2. DataFrame 표시하기') # 텍스트 출력
-31 df = pd.DataFrame({ # DataFrame 생성
-32 '이름': ['홍길동', '이순신', '강감찬'],
-33 '나이': [20, 45, 35]
-34 })
+st.write('# 1. Markdown 텍스트 작성하기')
+
+# Markdown 문법으로 작성된 문장 출력
+st.markdown(
+    '''
+    # 마크다운 헤더1
+    - 마크다운 목록1. **굵게** 표시
+    - 마크다운 목록2. *기울임* 표시
+    - 마크다운 목록2-1
+    - 마크다운 목록2-2
+
+    ## 마크다운 헤더2
+    - [네이버](https://naver.com)
+    - [구글](https://google.com)
+    
+    ### 마크다운 헤더3
+    일반 텍스트
+    '''
+    )
+
+# DataFrame 출력
+import pandas as pd # pandas 라이브러리 임포트
+
+st.write('# 2. DataFrame 표시하기') # 텍스트 출력
+df = pd.DataFrame({ # DataFrame 생성
+    '이름': ['홍길동', '이순신', '강감찬'],
+    '나이': [20, 45, 35]
+})
+
+st.dataframe(df) # DataFrame 출력
+
+# 그래프 출력
+import numpy as np # numpy 라이브러리 임포트
+
+st.write('# 3. 그래프 표시하기') # 텍스트 출력
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"]) # DataFrame 생성
+
+st.bar_chart(chart_data) # 바 차트 출력
+
+# 이미지 출력
+from PIL import Image # 이미지 처리를 위한 PIL 라이브러리 임포트
+
+st.write('# 4. 이미지 표시하기') # 텍스트 출력
+img = Image.open('python.png') # 이미지 파일 열기
+st.image(img, width=300) # 이미지 출력
+
+
+
